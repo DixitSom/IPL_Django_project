a01f6e5689689a98cefc9c36ad9aac154f7d83de
Rohan Dixit <rohank.dixit9625@gmail.com>
2021-09-21 14:56:14 +0530

    Deliuvery Model added

--- ipl/charts/models.py
+++ ipl/charts/models.py
@@ -25,3 +25,28 @@ class Match(models.Model):
 
     def __str__(self) -> str:
         return str(self.id)
+
+class Delivery(models.Model):
+
+    match_id = models.IntegerField()
+    match = models.ForeignKey(Match, on_delete=models.CASCADE)
+    inning = models.IntegerField()
+    batting_team = models.CharField(max_length=10)
+    bowling_team = models.CharField(max_length=10)
+    over = models.IntegerField()
+    ball = models.IntegerField()
+    batsman = models.CharField(max_length=10)
+    non_striker = models.CharField(max_length=10)
+    bowler = models.CharField(max_length=10)
+    is_super_over = models.BooleanField()
+    wide_runs = models.IntegerField()
+    bye_runs = models.IntegerField()
+    legbye_runs = models.IntegerField()
+    noball_runs = models.IntegerField()
+    penalty_runs = models.IntegerField()
+    batsman_runs = models.IntegerField()
+    extra_runs = models.IntegerField()
+    total_runs = models.IntegerField()
+    player_dismissed = models.CharField(max_length=10, null=True)
+    dismissal_kind = models.CharField(max_length=10)
+    fielder = models.CharField(max_length=10)
