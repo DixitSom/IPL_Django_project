ca4aa95cd07f38dba8738c6f3664c3cb0848270b
Rohan Dixit <rohank.dixit9625@gmail.com>
2021-09-21 13:05:01 +0530

    Import Fixed

--- ipl/charts/management/commands/import.py
+++ ipl/charts/management/commands/import.py
@@ -4,7 +4,7 @@ from django.core.management.base import BaseCommand
 from django.apps import apps
 from django.db import transaction
 
-from .models import Match
+from ipl.charts.models import Match
 
 
 # This is the class to create command for importing dataset.
