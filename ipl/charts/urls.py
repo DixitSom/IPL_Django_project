34b0654c631f8d62f8061763ee8ea3baf190753d
Rohan Dixit <rohank.dixit9625@gmail.com>
2021-09-21 20:58:55 +0530

    URL path added

---
+++ ipl/charts/urls.py
@@ -0,0 +1,7 @@
+from ipl.charts.views import won_matches_per_year
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('/json/matches_win', won_matches_per_year)
+]
