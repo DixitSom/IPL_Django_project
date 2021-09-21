e040c52310a37a4c92495c5acf0836da666a7648
Rohan Dixit <rohank.dixit9625@gmail.com>
2021-09-21 21:19:01 +0530

    URLS update

--- ipl/charts/urls.py
+++ ipl/charts/urls.py
@@ -1,7 +1,8 @@
-from .views import won_matches_per_year
+from .views import extra_runs_by_teams, won_matches_per_year
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('json/matches_win/', won_matches_per_year, name='won_matches')
+    path('json/matches_win/', won_matches_per_year, name='won_matches'),
+    path('json/extra_runs/<str:year>', extra_runs_by_teams, name='extra_runs')
 ]
